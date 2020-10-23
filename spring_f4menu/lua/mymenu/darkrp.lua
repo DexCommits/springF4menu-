59872b71689fa87d90781debf6bde252e3b50100
Dex <gryphogrypho@gmail.com>
2020-10-23 14:49:39 +0100

    Delete darkrp.lua

--- spring_f4menu/lua/mymenu/darkrp.lua
+++
@@ -1,9 +0,0 @@
-local function OverrideDarkRPMenu()
-    function DarkRP.openMenu() end
-end
-
-if (DarkRP) then
-   OverrideDarkRPMenu()
-else
-   hook.Add("DarkRPFinishedLoading", "F4Menu.OverrideItemLimit", OverrideDarkRPMenu)
-end
