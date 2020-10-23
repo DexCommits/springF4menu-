ec812ffd5e498b1f7fdfbe274480bed021a59be0
Dex <gryphogrypho@gmail.com>
2020-10-23 14:50:02 +0100

    Delete font.lua

--- spring_f4menu/lua/mymenu/font.lua
+++
@@ -1,7 +0,0 @@
-function CODEXLIST:CreateFont(name, size, weight)
-    surface.CreateFont("CODEXLIST." .. name, {
-      font = "Agency FB",
-      size = size or 16,
-      weight = weight or 580
-     })
-     end
